7e18f8537d4ebee1431c23e0f637e527e1157726
Pavlos Vinieratos <pvinis@gmail.com>
2016-01-20 21:15:26 +0100

    posting

--- src/snk/core.clj
+++ src/snk/core.clj
@@ -1,13 +1,27 @@
 (ns snk.core)
 
-
-
-
-
-
-
 (defn snk-reading
   [messages user]
   (clojure.string/join (map (fn [message]
                               (str (first message) " (" (second message) " sec)\n"))
                             (messages user))))
+
+(defn snk-posting
+  [messages user message]
+  (let [old-messages messages
+        new-messages (assoc old-messages user (conj (old-messages user []) [message snk-current-time]))]
+    new-messages))
+
+
+
+
+;;not ready stuff
+(def snk-current-time 40)
+
+(snk-posting msg :bob "how")
+
+(def msg {:alice [["hello" 10]
+                  ["hello again" 25]]
+          :bob [["hello from me too" 15]]})
+
+;(update-in msg [:charlie] (fnil conj []) ["third" 40])
